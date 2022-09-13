bc7335c05725c1bd0fa2308dd43cd1e362197c51
Luis Encina <luisencina369@gmail.com>
2022-09-12 20:29:46 -0400

    try_except_raise error

--- 2-intermedio/11-debugging.py
+++ 2-intermedio/11-debugging.py
@@ -1,15 +1,24 @@
 def divisors(num):
     divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError("Error: Sólo se aceptan números positivos")
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisors.append(i)
+        
+        return divisors
+    except ValueError as ve:
+        return ve
+
 
 def run():
-    num = int(input("Ingrese un número: "))
-    print(divisors(num))
-    print("Terminó el programa.")
+    try:
+        num = int(input("Ingrese un número: "))
+        print(divisors(num))
+        print("Terminó el programa.")
+    except ValueError:
+        print("Sólo se aceptan números.")
 
 
 if __name__ == "__main__":
