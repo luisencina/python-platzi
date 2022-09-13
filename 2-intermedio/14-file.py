e45c6e140b033c6f192a8dc5fbd18be79e1f1d95
Luis Encina <luisencina369@gmail.com>
2022-09-13 19:57:48 -0400

    working with files

---
+++ 2-intermedio/14-file.py
@@ -0,0 +1,26 @@
+import numbers
+
+
+def read():
+    numbers = []
+    with open("./files/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    
+    print(numbers)
+
+
+def write():
+    names = ["Facudo", "Luis", "María"]
+    with open("./files/names.txt", "w", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+def run():
+    read()
+    write()
+
+
+if __name__ == "__main__":
+    run()
