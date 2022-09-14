945d884513db44bf1bd20c03018d76ed88ac8ba2
Luis Encina <luisencina369@gmail.com>
2022-09-13 21:47:29 -0400

    hangman game

---
+++ 2-intermedio/15-hangman.py
@@ -0,0 +1,85 @@
+import os
+import random
+
+
+def normalize(word):
+    replacements = {'á': 'a','é': 'e','í': 'i','ó': 'o','ú': 'u'}
+    new_word = ""
+    for c in word:
+        if c in replacements:
+            new_word += replacements[c]
+        else:
+            new_word += c
+
+    return new_word
+
+ 
+
+def read():
+    words = []
+    with open("./files/data.txt", "r", encoding="utf-8") as f:
+        words = [normalize(data.replace("\n", "").lower()) for data in f]
+
+    return random.choice(words)
+
+
+def unMascarate(character, word, userWord):
+    userWord = [c for c in userWord]
+    list = [c for c in word]
+    c = []
+
+    for i in range(0, len(list)):
+        if list[i] == character:
+            c.append(i)
+    
+    for i in c:
+        userWord[i] = character
+
+    return ''.join(userWord)
+
+def isCorrect(character, word):
+    if character in word:
+        return True
+    return False
+
+
+def game(word):
+
+    userWord =  "_" * len(word)
+    intentos = 0
+    
+    while userWord != word:
+        os.system('clear')
+        print("N de intentos: ", intentos)
+        try:
+            print(userWord)
+            character = str(input("Inserte una letra: "))
+            assert character != "", "Debes ingresar una letra" 
+            if isCorrect(character, word):
+                userWord = unMascarate(character, word, userWord)
+            else:
+                intentos += 1
+            
+            if intentos >= 10:
+                print("Perdiste, La palabra era: ", word)
+                exit(0)
+
+        except AssertionError as ae:
+            print(ae)
+            character = str(input("Inserte una letra: "))
+        except KeyboardInterrupt:
+            print("\nAdiós!")
+            exit(0)
+        
+
+    print("Ganaste! Palabra: ", word)
+
+
+def run():
+    word = read()
+    game(word)
+    
+    
+
+if __name__ == "__main__":
+    run()
