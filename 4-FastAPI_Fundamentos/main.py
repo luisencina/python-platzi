70e7e3fb0a3e8248a84de69d2431e90c9821268a
Luis Encina <luisencina369@gmail.com>
2022-11-07 20:43:21 -0300

    put operations

---
+++ 4-FastAPI_Fundamentos/main.py
@@ -0,0 +1,88 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
+from fastapi import FastAPI
+from fastapi import Body, Query, Path
+
+app = FastAPI()
+
+
+# Models
+
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
+@app.get("/")
+def home():
+    return {"Hello": "World"}
+
+
+# Request and Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+
+
+# Validations: Path Parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+        person_id: int = Path(
+            ...,
+            gt=0,
+            title="Person id",
+            description="It's required"
+        )
+):
+    return {person_id: "OK"}
+
+
+# Validations: Query Parameters
+@app.get("/person/detail")
+def show_person(
+        name: Optional[str] = Query(
+            None,
+            min_length=1,
+            max_length=50,
+            title="Person Name",
+            description="This is the persona name. It's between 1 and 50 characters."
+        ),
+        age: int = Query(
+            ...,
+            title="Person age",
+            description="This is the person age. It's required"
+        )
+):
+    return {name: age}
+
+
+# Validations: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+        person_id: int = Path(
+            ...,
+            title="Person ID",
+            description="This is the person ID",
+            gt=0
+        ),
+        person: Person = Body(...),
+        location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
